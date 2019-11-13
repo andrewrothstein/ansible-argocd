a67f34bef0a718fd154c40b42c6e270e3cb56d2c
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-11-13 11:24:12 -0500

    initial commit

---
+++ dl-checksum.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+DIR=~/Downloads
+MIRROR=https://github.com/argoproj/argo-cd/releases/download
+APP=argocd
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local platform="$os-$arch"
+    local url=$MIRROR/$ver/$APP-$platform
+    local lfile=$DIR/$APP-$ver-$platform
+
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+}
+
+dlver () {
+    local ver=$1
+    printf "  %s:\n" $ver
+    dl $ver linux amd64
+    dl $ver darwin amd64
+    dl $ver windows amd64
+}
+
+dlver ${1:-v1.3.0}

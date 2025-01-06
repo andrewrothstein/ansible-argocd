62b34feb0bbeb4f0750278b6c3d468a4b69d822c
Andrew Rothstein <andrew.rothstein@gmail.com>
2025-01-06 17:03:09 +0000

    argocd:2.13.3

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -6,33 +6,39 @@ APP=argocd
 
 dl()
 {
-    local ver=$1
-    local os=$2
-    local arch=$3
-    local dotexe=${4:-}
+    local lchecksums=$1
+    local ver=$2
+    local os=$3
+    local arch=$4
+    local dotexe=${5:-}
     local platform="$os-$arch"
-    local url="${MIRROR}/${ver}/${APP}-${platform}${dotexe}"
-    local lfile="${DIR}/${APP}-${ver}-${platform}${dotexe}"
-
-    if [ ! -e $lfile ];
-    then
-        curl -sSLf -o $lfile $url
-    fi
+    local file="${APP}-${platform}${dotexe}"
+    local url="${MIRROR}/${ver}/${file}"
 
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
 }
 
 dlver () {
     local ver=$1
+
+    # https://github.com/argoproj/argo-cd/releases/download/v2.13.3/cli_checksums.txt
+    local checksums_url="${MIRROR}/${ver}/cli_checksums.txt"
+    local lchecksums="${DIR}/argocd_${ver}_checksums.txt"
+    if [ ! -e "${lchecksums}" ];
+    then
+        curl -sSLf -o "${lchecksums}" "${checksums_url}"
+    fi
+
+    printf "  # %s\n" $checksums_url
     printf "  %s:\n" $ver
-    dl $ver linux amd64
-    dl $ver linux arm64
-    dl $ver linux ppc64le
-    dl $ver linux s390x
-    dl $ver darwin amd64
-    dl $ver darwin arm64
-    dl $ver windows amd64 .exe
+    dl $lchecksums $ver linux amd64
+    dl $lchecksums $ver linux arm64
+    dl $lchecksums $ver linux ppc64le
+    dl $lchecksums $ver linux s390x
+    dl $lchecksums $ver darwin amd64
+    dl $lchecksums $ver darwin arm64
+    dl $lchecksums $ver windows amd64 .exe
 }
 
-dlver ${1:-v2.13.0}
+dlver ${1:-v2.13.3}
